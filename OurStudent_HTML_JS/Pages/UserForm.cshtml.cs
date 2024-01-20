4b22af708e8352ecb21ab6e7b4081869950a8753
Rohan Dhok <rohandhok6@gmail.com>
2024-01-20 14:48:05 +0530

    frontend

---
+++ OurStudent_HTML_JS/Pages/UserForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OurStudent_HTML_JS.Pages
+{
+    public class UserFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
