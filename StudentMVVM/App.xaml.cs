97c5da1ec4038944c12ad6e304b241218ebdac9b
Rohan Dhok <rohandhok6@gmail.com>
2024-02-22 19:10:42 +0530

    Today Day end

---
+++ StudentMVVM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
